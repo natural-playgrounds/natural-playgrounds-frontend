2bc8c43f324f7d70a30ee5dd10e6e28d83a1b5ab
John Vavul <jvavul@gmail.com>
2026-06-24 11:07:21 -1000

    clear cart on logout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,45 @@
 import "../styles/globals.css";
-import { CartContext, useCartState } from "../hooks/use-cart-slide.js";
+import { useEffect } from "react";
+import { CartContext, useCartSlide, useCartState } from "../hooks/use-cart-slide.js";
 import Layout from "../components/layout";
-import { CartProvider } from "react-use-cart";
+import { CartProvider, useCart } from "react-use-cart";
 import { Toaster } from "react-hot-toast";
+
+function CartLogoutSync() {
+  const { emptyCart } = useCart();
+  const { updateCartSlide } = useCartSlide();
+
+  useEffect(() => {
+    const clearCart = () => {
+      emptyCart();
+      updateCartSlide(false);
+    };
+
+    const handleStorage = (event) => {
+      if (event.key === "logout") {
+        clearCart();
+      }
+    };
+
+    window.addEventListener("logout", clearCart);
+    window.addEventListener("storage", handleStorage);
+
+    return () => {
+      window.removeEventListener("logout", clearCart);
+      window.removeEventListener("storage", handleStorage);
+    };
+  }, [emptyCart, updateCartSlide]);
+
+  return null;
+}
+
 function MyApp({ Component, pageProps }) {
   const cart = useCartState();
   return (
     <>
       <CartProvider>
         <CartContext.Provider value={cart}>
+          <CartLogoutSync />
           <Layout>
             <Component {...pageProps} />
           </Layout>
